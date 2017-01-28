32dcfa512aac90ba5d994f0d4326296673c519c9
Luca <lucamug@gmail.com>
2017-01-28 23:03:41 +0100

    fixed tab problem

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,14 @@
 import React, { PropTypes } from 'react';
 import { footer } from '../styles/footer.sass';
 const App = ({ children }) =>
-	< div > { children } < footer className = { footer } >
-	< /footer> < /div>;
+    <div>
+        { children }
+        <footer className={footer}>
+        </footer>
+    </div>;
 
 App.propTypes = {
-	children: PropTypes.object
+    children: PropTypes.object
 };
 
 export default App;
